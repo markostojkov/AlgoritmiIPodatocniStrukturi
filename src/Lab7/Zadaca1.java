07ba0a46eb8f15f4611b88ffdaba0ab66f50bc7b
Marko Stojkov <mst@mca.dev>
2020-12-28 19:55:23 +0100

    LAB 7

---
+++ src/Lab7/Zadaca1.java
@@ -0,0 +1,224 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+interface Tree<E> {
+    ////////////Accessors ////////////
+
+    public Node<E> root();
+
+    public Node<E> parent(Node<E> node);
+
+    public int childCount(Node<E> node);
+
+    ////////////Transformers ////////////
+    public void makeRoot(E elem);
+
+    public Node<E> addChild(Node<E> node, E elem);
+
+    public void remove(Node<E> node);
+
+    ////////////Iterator ////////////
+    public Iterator<E> children(Node<E> node);
+    
+}
+
+interface Node<E> {
+
+    	public E getElement();
+
+    	public void setElement(E elem);
+}
+
+
+class SLLTree<E> implements Tree<E> {
+    
+    public SLLNode<E> root;
+
+    public SLLTree() {
+        root = null;
+    }
+
+    public Node<E> root() {
+        return root;
+    }
+
+    public Node<E> parent(Node<E> node) {
+        return ((SLLNode<E>) node).parent;
+    }
+
+    public int childCount(Node<E> node) {
+        SLLNode<E> tmp = ((SLLNode<E>) node).firstChild;
+        int num = 0;
+        while (tmp != null) {
+            tmp = tmp.sibling;
+            num++;
+        }
+        return num;
+    }
+
+    public void makeRoot(E elem) {
+        root = new SLLNode<E>(elem);
+    }
+
+    public Node<E> addChild(Node<E> node, E elem) {
+        SLLNode<E> tmp = new SLLNode<E>(elem);
+        SLLNode<E> curr = (SLLNode<E>) node;
+        tmp.sibling = curr.firstChild;
+        curr.firstChild = tmp;
+        tmp.parent = curr;
+        return tmp;
+    }
+
+    public void remove(Node<E> node) {
+        SLLNode<E> curr = (SLLNode<E>) node;
+        if (curr.parent != null) {
+            if (curr.parent.firstChild == curr) {
+                // The node is the first child of its parent
+                // Reconnect the parent to the next sibling
+                curr.parent.firstChild = curr.sibling;
+            } else {
+                // The node is not the first child of its parent
+                // Start from the first and search the node in the sibling list and remove it
+                SLLNode<E> tmp = curr.parent.firstChild;
+                while (tmp.sibling != curr) {
+                    tmp = tmp.sibling;
+                }
+                tmp.sibling = curr.sibling;
+            }
+        } else {
+            root = null;
+        }
+    }
+
+    class SLLTreeIterator<T> implements Iterator<T> {
+
+        SLLNode<T> start, current;
+
+        public SLLTreeIterator(SLLNode<T> node) {
+            start = node;
+            current = node;
+        }
+
+        public boolean hasNext() {
+            return (current != null);
+        }
+
+        public T next() throws NoSuchElementException {
+            if (current != null) {
+                SLLNode<T> tmp = current;
+                current = current.sibling;
+                return tmp.getElement();
+            } else {
+                throw new NoSuchElementException();
+            }
+        }
+
+        public void remove() {
+            if (current != null) {
+                current = current.sibling;
+            }
+        }
+    }
+
+    public Iterator<E> children(Node<E> node) {
+        return new SLLTreeIterator<E>(((SLLNode<E>) node).firstChild);
+    }
+    
+    void printTreeRecursive(Node<E> node, int level) {
+        if (node == null)
+            return;
+        int i;
+        SLLNode<E> tmp;
+
+        for (i=0;i<level;i++)
+            System.out.print(" ");
+        System.out.println(node.getElement().toString());
+        tmp = ((SLLNode<E>)node).firstChild;
+        while (tmp != null) {
+            printTreeRecursive(tmp, level+1);
+            tmp = tmp.sibling;
+        }
+    }
+    
+    public void printTree() {
+        printTreeRecursive(root, 0);
+    }
+    
+}
+
+class SLLNode<P> implements Node<P> {
+
+    // Holds the links to the needed nodes
+    public SLLNode<P> parent, sibling, firstChild;
+    // Hold the data
+    public P element;
+
+    public SLLNode(P o) {
+        element = o;
+        parent = sibling = firstChild = null;
+    }
+
+    public P getElement() {
+        return element;
+    }
+
+    public void setElement(P o) {
+        element = o;
+    }
+}
+
+public class WindowsExplorer {
+    
+    public static void main(String[] args) throws Exception {
+        int i,j,k;
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(br.readLine());
+        String commands[] = new String[N];
+        
+        for (i=0;i<N;i++)
+            commands[i] = br.readLine();
+            
+        br.close();
+        
+        SLLTree<String> tree = new SLLTree<String>();
+        tree.makeRoot("c:");
+        Node<String> currentRoot = tree.root();
+
+        for (String command : commands) {
+            String[] commandAndName = command.split(" ");
+
+            switch(commandAndName[0]) {
+                case "CREATE":
+                    currentRoot = tree.addChild(currentRoot, commandAndName[1]);    
+                    break;
+                case "OPEN":
+                    currentRoot = tree.root
+                    break;
+                case "DELETE":
+                    tree.remove(new SLLNode<String>(commandAndName[1]));
+                    break;
+                case "BACK":
+                    tree.makeRoot(tree.root().getElement());
+                    break;
+                case "PATH":
+                    SLLNode<String> pathRoot = (SLLNode<String>) currentRoot;
+                    String path = "";
+                    while(pathRoot != null) {
+                        path += pathRoot.getElement() + "\\";
+                        pathRoot = pathRoot.parent;
+                    }
+                    System.out.println(path);
+                    break;
+                case "PRINT":
+                    tree.printTree();
+                    break;
+            }
+        }
+    }
+}
---
+++ src/Lab7/Zadaca3.java
@@ -0,0 +1,159 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class BNode<E> {
+    
+    public E info;
+    public BNode<E> left;
+    public BNode<E> right;
+    
+    static int LEFT = 1;
+    static int RIGHT = 2;
+    
+    public BNode(E info) {
+        this.info = info;
+        left = null;
+        right = null;
+    }
+    
+    public BNode() {
+        this.info = null;
+        left = null;
+        right = null;
+    }
+    
+    public BNode(E info, BNode<E> left, BNode<E> right) {
+        this.info = info;
+        this.left = left;
+        this.right = right;
+    }
+    
+}
+
+class BTree<E extends Comparable<E>> {
+    
+    public BNode<E> root;
+    
+    public BTree() {
+        root = null;
+    }
+    
+    public BTree(E info) {
+        root = new BNode<E>(info);
+    }
+    
+    public void makeRoot(E elem) {
+        root = new BNode(elem);
+    }
+    
+    public void makeRootNode(BNode<E> node) {
+        root = node;
+    }
+    
+    public BNode<E> addChild(BNode<E> node, int where, E elem) {
+        
+        BNode<E> tmp = new BNode<E>(elem);
+        
+        if (where == BNode.LEFT) {
+            if (node.left != null)  // veke postoi element
+                return null;
+            node.left = tmp;
+        } else {
+            if (node.right != null) // veke postoi element
+                return null;
+            node.right = tmp;
+        }
+        
+        return tmp;
+    }
+    
+    public BNode<E> addChildNode(BNode<E> node, int where, BNode<E> tmp) {
+        
+        if (where == BNode.LEFT) {
+            if (node.left != null)  // veke postoi element
+                return null;
+            node.left = tmp;
+        } else {
+            if (node.right != null) // veke postoi element
+                return null;
+            node.right = tmp;
+        }
+        
+        return tmp;
+    }
+
+}
+
+public class BinaryTreeSum {
+    
+    public static int getLeftSum(BNode<Integer> node, int sum, int smallerThan) {
+        if (node == null) {
+            return sum;
+        }
+
+        sum += getLeftSum(node.left, smallerThan > node.info ? node.info : 0, smallerThan);
+        sum += getLeftSum(node.right, smallerThan > node.info ? node.info : 0, smallerThan);
+        return sum;
+    }
+
+    public static int getRightSum(BNode<Integer> node, int sum, int biggerThan) {
+        if (node == null) {
+            return sum;
+        }
+
+        sum += getRightSum(node.left, biggerThan < node.info ? node.info : 0, biggerThan);
+        sum += getRightSum(node.right, biggerThan < node.info ? node.info : 0, biggerThan);
+        return sum;
+    }
+
+    public static void main(String[] args) throws Exception {
+        int i, j, k;
+        int index;
+        String action;
+        
+        String line;
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+
+        BNode<Integer> nodes[] = new BNode[N];
+        BTree<Integer> tree = new BTree<Integer>();
+        
+        for (i=0;i<N;i++)
+            nodes[i] = new BNode<Integer>();
+        
+        for (i = 0; i < N; i++) {
+            line = br.readLine();
+            st = new StringTokenizer(line);
+            index = Integer.parseInt(st.nextToken());
+            nodes[index].info = Integer.parseInt(st.nextToken());
+            action = st.nextToken();
+            if (action.equals("LEFT")) {
+                tree.addChildNode(nodes[Integer.parseInt(st.nextToken())], BNode.LEFT, nodes[index]);
+            } else if (action.equals("RIGHT")) {
+                tree.addChildNode(nodes[Integer.parseInt(st.nextToken())], BNode.RIGHT, nodes[index]);    
+            } else {
+                // this node is the root
+                tree.makeRootNode(nodes[index]);
+            }
+        }
+
+        int baranaVrednost=Integer.parseInt(br.readLine());
+        
+        br.close();
+        
+        BNode<Integer> node = nodes[0];
+
+        for (BNode<Integer> bNode : nodes) {
+            if (bNode.info == baranaVrednost) node = bNode;
+        }
+
+        int leftSum = 0;
+        int rightSum = 0;
+
+        System.out.println(getLeftSum(node.left, 0, node.info) / 2 + " " + getRightSum(node.right, 0, node.info) / 2);
+    }
+}
